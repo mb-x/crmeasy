039f6eecaa079e6baa76d1eb65aac2fb38101a90
bengrich <webmasterbengrich@gmail.com>
2017-06-26 22:30:04 +0000

    created the marketing app

--- crmapp/settings.py
+++ crmapp/settings.py
@@ -29,7 +29,7 @@ def get_env_variable(var_name):
 # ENV_ROLE = get_env_variable('ENV_ROLE')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crmapp.marketing',
 ]
 
 MIDDLEWARE = [
@@ -141,8 +142,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
